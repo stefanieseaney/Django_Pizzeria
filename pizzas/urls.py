786ab9766570de7f3d3242cd17109edf3280293e
Stefanie Seaney <stefanie.seaney@gmail.com>
2021-04-23 15:47:04 -0500

    Created default homepage.

---
+++ pizzas/urls.py
@@ -0,0 +1,10 @@
+"""Defines URL patterns for pizzas"""
+from django.urls import path
+from . import views
+
+app_name = "pizzas"
+
+urlpatterns = [
+    # Home page
+    path("", views.index, name="index"),
+]
--- pizzas/views.py
+++ pizzas/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Pizza
 
 # Create your views here.
+
+
+def index(request):
+    """Homepage."""
+    return render(request, "pizzas/index.html")
