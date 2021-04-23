aa99bc63366557847bccf4225b55d368414ba99b
Stefanie Seaney <stefanie.seaney@gmail.com>
2021-04-23 16:48:06 -0500

    Voila!

--- pizzas/views.py
+++ pizzas/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Pizza
+from .forms import CommentForm
 
 # Create your views here.
 
@@ -20,6 +21,16 @@ def pizza(request, pizza_id):
     """Individual pizza page."""
     pizza = Pizza.objects.get(id=pizza_id)
     toppings = pizza.topping_set.order_by("name")
+    comments = pizza.comment_set.order_by("-date_added")
 
-    context = {"pizza": pizza, "toppings": toppings}
+    form = CommentForm()
+    if request.method == "POST":
+        form = CommentForm(data=request.POST)
+        if form.is_valid():
+            new_comment = form.save(commit=False)
+            new_comment.pizza = pizza
+            new_comment.save()
+            return redirect("pizzas:pizza", pizza_id=pizza_id)
+
+    context = {"pizza": pizza, "toppings": toppings, "comments": comments, "form": form}
     return render(request, "pizzas/pizza.html", context)
